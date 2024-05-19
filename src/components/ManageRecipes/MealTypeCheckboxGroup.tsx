9843be19b57507c8e35175fe2563278e4eae5ff4
Jacob DeWar <whiterbread@gmail.com>
2024-05-19 16:43:25 -0400

    fix defaultmealtype

--- src/components/ManageRecipes/MealTypeCheckboxGroup.tsx
+++ src/components/ManageRecipes/MealTypeCheckboxGroup.tsx
@@ -13,23 +13,23 @@ export default function MealTypeCheckboxGroup({ validMealTypes }: MealTypeCheckb
                         justifyContent="space-evenly"
                         alignItems="center">
                         <Grid item >
-                            <FormControlLabel control={<Checkbox defaultChecked name="breakfast" />} label="Breakfast" />
+                            <FormControlLabel control={<Checkbox defaultChecked={validMealTypes?.breakfast} name="breakfast" />} label="Breakfast" />
                         </Grid>
                         <Grid item >
-                            <FormControlLabel control={<Checkbox defaultChecked name="lunch" />} label="Lunch" />
+                            <FormControlLabel control={<Checkbox defaultChecked={validMealTypes?.lunch} name="lunch" />} label="Lunch" />
                         </Grid>
                         <Grid item>
-                            <FormControlLabel control={<Checkbox defaultChecked name="dinner" />} label="Dinner" />
+                            <FormControlLabel control={<Checkbox defaultChecked={validMealTypes?.dinner} name="dinner" />} label="Dinner" />
                         </Grid>
                     </Grid>
                     <Grid container
                         justifyContent="space-evenly"
                         alignItems="center">
                         <Grid item >
-                            <FormControlLabel control={<Checkbox name="snack" />} label="Snack" />
+                            <FormControlLabel control={<Checkbox name="snack" defaultChecked={validMealTypes?.snack} />} label="Snack" />
                         </Grid>
                         <Grid item >
-                            <FormControlLabel control={<Checkbox name="dessert" />} label="Dessert" />
+                            <FormControlLabel control={<Checkbox name="dessert" defaultChecked={validMealTypes?.dessert} />} label="Dessert" />
                         </Grid>
                     </Grid>
         </Stack>
